ab9ec95cc7ff43ad1d82146ef69abb0f4305cd3f
Timur_2101 <timurka260197@gmil.com>
2023-08-07 18:17:39 +0200

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+int[] array = GetArray(7, 100, 1000);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    count += array[i] % 2 == 0 ? 1 : 0;
+}
+Console.WriteLine(count);
+
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
