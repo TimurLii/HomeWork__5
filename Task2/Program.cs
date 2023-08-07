a5dddc52612de73e2b26d12c1655ffb5159fe97f
Timur_2101 <timurka260197@gmil.com>
2023-08-07 18:19:59 +0200

    Task

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int[] array = GetArray(8, 2, 10);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int sum = 0;
+int i = 1;
+while (i < array.Length)
+{
+    sum += array[i];
+    i += 2;
+}
+Console.WriteLine(sum);
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив целых чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3, 4, 1, 77, 65] => 77 - 1 = 76
+int[] array = GetArray(5, 1, 99);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+
+
+int min = Int32.MaxValue;
+int max = Int32.MinValue;
+for (int i = 0; i < array.Length; i++)
+
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine($"Максимальное значение {max},минимальное значение {min}. Разница равна {max - min}");
+
+
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
